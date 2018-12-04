a7794296564611a97c1f3a7eb037f2bd1bfdc9a9
Francois Gauthier-Clerc <francois@gauthier-clerc.fr>
2018-12-04 18:29:15 +0100

    First commit about map obstacle

---
+++ MapObstacle/Map.py
@@ -0,0 +1,21 @@
+from math import exp, hypot
+
+class Map:
+    def __init__(self, length, width, alpha=1, beta=0):
+        self.length = length
+        self.width  = width
+        self.alpha = alpha
+        self.beta = beta
+
+    def get_force(self, position):
+        x , y = position
+        wall_vect = ((0, y), (x, 0), (self.width,y), (x,self.length))
+        result = 0
+        for (x_wall, y_wall) in wall_vect:
+            result += exp(-hypot(x_wall-x, y_wall-y)*self.alpha)*self.beta
+        
+        return result
+
+
+
+
---
+++ MapObstacle/testdynAlpha.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D 
+import matplotlib.animation as animation
+from Map import *
+import pymp
+alphas = [1,0.5,0.1,0.05,0.01,0.005,0.001,0.0005,0.0001]
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Make data.
+X = np.arange(0, 2000, 10)
+Y = np.arange(0, 3000, 10)
+X, Y = np.meshgrid(X, Y)
+Zs = [np.zeros_like(X,dtype=np.float) for i in range(len(alphas))]
+maps = [ Map(3000,2000,alpha,1) for alpha in alphas]
+with pymp.Parallel(4) as p:
+    for alpha in range(len(alphas)):
+        for i in range(X.shape[0]):
+            for k in range(X.shape[1]):
+                Zs[alpha][i,k] = maps[alpha].get_force((X[i,k],Y[i,k]))
+
+
+def update_plot(frame_number, zarray, plot):
+    plot[0].remove()
+    plot[0] = ax.plot_surface(X, Y, zarray[frame_number], cmap="magma")
+    ax.set_title("alpha : "+ str(alphas[frame_number]))
+
+
+plot = [ax.plot_surface(X, Y, Zs[0], color='0.75', rstride=1, cstride=1)]
+ax.set_zlim(0,np.max(np.concatenate(Zs)))
+animate = animation.FuncAnimation(fig, update_plot, len(alphas), fargs=(Zs, plot),interval=1000)
+plt.show()
