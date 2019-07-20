90988424268927f88c2ae880fae252c693f61abd
Francois Gauthier-Clerc <francois@gauthier-clerc.fr>
2019-07-20 15:33:17 +0200

    clean source, and fix mistake on compute func

--- PotAvoidance/path/compute.py
+++ PotAvoidance/path/compute.py
@@ -3,7 +3,7 @@ from math import hypot
 def compute(fields, start, target, max_it=500, tol=10):
     path = list()
     it = 0 
-    robot_pos = start
+    robot_pos = list(start)
     robot_vel = [0, 0]
     while(hypot(robot_pos[0]-target[0], robot_pos[1]- target[1])>tol and it<max_it):
         robot_pos[0], robot_pos[1] =  robot_pos[0]+robot_vel[0], robot_pos[1]  + robot_vel[1]
